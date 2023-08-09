fb6208c7b5f5b2f7973b28fb3a49737c0767bd43
Prince Aleksey <chirkinaa@pgk.ru>
2023-08-09 18:47:41 +0300

    creating a test

--- lessons/conftest.py
+++ lessons/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selene import browser
 
+
 @pytest.fixture
 def browser_setup():
     driver = webdriver.Chrome()
@@ -14,19 +15,21 @@ def browser_setup():
     browser.close()
 
 
-@pytest.fixture(scope='function', autouse=True) #scope='function' вызывается для функции, autouse=True чтобы вызывалась автоматически
+@pytest.fixture(scope='function',
+                autouse=True)  # scope='function' вызывается для функции, autouse=True чтобы вызывалась автоматически
 def browser_management():
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.config.driver_name = 'chrome'
     browser.config.timeout = 6.0
+    # browser.config.click_by_js = True
 
-    #browser.config.type_by_js = True
-    #browser.config.click_by_js = True
-#driver_options = webdriver.ChromeOptions()
-    #driver_options.add_argument("--headless")
-   #browser.config.driver_options = driver_options
+    # driver_options = webdriver.ChromeOptions()
+    # browser.config.driver_options = driver_options
+    # driver_options.add_argument("--headless")
 
-    #yield # передает выполнение тесту
+    # browser.config.type_by_js = True
+    # driver_options = webdriver.ChromeOptions()
 
+    yield  # передает выполнение тесту
 
-    #browser.quit()
+    browser.quit()
--- lessons/test_fifth_lesson.py
+++ lessons/test_fifth_lesson.py
@@ -1,41 +1,60 @@
 from selene import browser, be, have, command
-
-
+import os
+from selenium.webdriver import Keys
 
 browser.config.hold_driver_at_exit = True
+
+
 def test_fifth_lesson():
     browser.open('/')
+    browser.element('img[href=https://demoqa.com/')
+    browser.element('img[src="/images/Toolsqa.jpg"]').should(be.visible)
+    browser.element('.main-header').should(have.text('Practice Form'))
+    browser.element('div.practice-form-wrapper h5').should(have.text('Student Registration Form'))
     browser.element('#firstName').should(have.attribute('placeholder').value('First Name'))
     browser.element('#lastName').should(have.attribute('placeholder').value('Last Name'))
     browser.element('#userEmail').should(have.attribute('placeholder').value('name@example.com'))
+    browser.all('input[name="gender"]').should(have.size(3))
+    browser.element('#userNumber').should(have.attribute('placeholder').value('Mobile Number'))
+    browser.element('label[for=hobbies-checkbox-1]').should(have.text('Sports'))
+    browser.element('label[for=hobbies-checkbox-2]').should(have.text('Reading'))
+    browser.element('label[for=hobbies-checkbox-3]').should(have.text('Music'))
+    browser.element('#subjectsInput').should(be.blank)
 
-
-    browser.element('#firstName').type('Александр')
-    browser.element('#lastName').type('Сергеевич')
-    browser.element('#userEmail').type('pushkin@natomsvete.net')
+    browser.element('#firstName').type('Ezekiel')
+    browser.element('#lastName').type('Romaguera')
+    browser.element('#userEmail').type('fakedata72553@gmail.com')
     browser.element('label[for="gender-radio-1"]').click()
     browser.element('label[for="gender-radio-2"]').click()
     browser.element('label[for="gender-radio-3"]').click()
     browser.element('label[for="gender-radio-1"]').click()
-    browser.all('input[name="gender"]').should(have.size(3))
+    browser.element('#userNumber').type('1234567890')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__day--031').click()
+    browser.element('#subjectsInput').type('English').press_enter()
+    browser.element('#subjectsInput').type('Arts').press_enter()
+    browser.element('label[for=hobbies-checkbox-1]').perform(command.js.click)
+    browser.element('label[for=hobbies-checkbox-2]').perform(command.js.click)
+    browser.element('label[for=hobbies-checkbox-3]').perform(command.js.click)
+    browser.element('#uploadPicture').send_keys(os.path.abspath('resources/test_img.jpg'))
+    browser.element('#currentAddress').type('841 Alvis Union')
+    browser.element('#state').click()
+    browser.element('#react-select-3-input').send_keys(Keys.ARROW_DOWN)
+    browser.element('#react-select-3-input').send_keys(Keys.ARROW_DOWN)
+    browser.element('#react-select-3-input').send_keys(Keys.ARROW_DOWN).press_enter()
+    browser.element('#city').click()
+    browser.element('#react-select-4-input').send_keys(Keys.ARROW_UP)
+    browser.element('#react-select-4-input').send_keys(Keys.ARROW_UP).press_enter()
     browser.element('#submit').should(have.css_property("cursor", "pointer"))
-
-
-
-
-
-'''
-    browser.element('#new-todo').should(be.blank)
-
-    browser.element('#new-todo').type('a').press_enter()
-    browser.element('#new-todo').type('b').press_enter()
-    browser.element('#new-todo').type('c').press_enter()
-    browser.all('#todo-list>li').should(have.size(3))
-
-    #browser.element('#new-todo').perform(command.js.click)
-    #save = browser.element('#new-todo').with_(click_by_js=True)
-    #save.click()
-    #save.click()
-    #save.click()
-    #save.click()
-    '''
+    browser.element('#submit').press_enter()
+    browser.element('.table-responsive').should(have.text('Ezekiel Romaguera' and
+                                                          'fakedata72553@gmail.com'
+                                                          and 'Male'
+                                                          and '1234567890'
+                                                          and '31 July,2023'
+                                                          and 'English, Arts'
+                                                          and 'Sports, Reading, Music'
+                                                          and 'test_img.jpg'
+                                                          and '841 Alvis Union'
+                                                          and 'Rajasthan Jaipur'))
+    browser.element('#closeLargeModal').click()
