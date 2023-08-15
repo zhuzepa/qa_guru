80d31ad13945f37c0f4bd50cc5f5880d1dc21d76
Prince Aleksey <chirkinaa@pgk.ru>
2023-08-15 23:32:42 +0300

    homework

--- lessons/sixth_leeson.py
+++ lessons/sixth_leeson.py
@@ -102,7 +102,6 @@ def go_to_companyname_homepage(page_url):
     actual_result = get_readable_string(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
-
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = get_readable_string(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
