ea91006cb6d18c16aae17b10530fb09ed42e5d35
Prince Aleksey <chirkinaa@pgk.ru>
2023-08-17 22:18:34 +0300

    update lesson

--- lessons/test_first_lesson.py
+++ lessons/test_first_lesson.py
@@ -1,7 +1,9 @@
 from selene.support.shared import browser
 from selene import be, have
 
+
 def test_google():
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.element('[name="q"]').should(be.blank).type('пгк').press_enter()
+    browser.element('[id="search"]').should(have.text(
+        'ПГК оказывает комплексные услуги по экспедированию грузов как внутри России, так и за рубежом в пределах колеи 1520.'))
