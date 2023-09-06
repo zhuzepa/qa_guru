937c1817f01d818f91d332771b0d16b447ab3a52
Prince Aleksey <chirkinaa@pgk.ru>
2023-09-07 00:59:26 +0300

    update

--- conftest.py
+++ conftest.py
@@ -1,13 +1,13 @@
-import pytest
-from selene import browser
-
-
-@pytest.fixture()
-def practice_form():
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 2000
-    browser.config.window_height = 1080
-
-    yield
-
-    browser.quit()
+# import pytest
+# from selene import browser
+#
+#
+# @pytest.fixture()
+# def practice_form():
+#     browser.config.base_url = 'https://demoqa.com'
+#     browser.config.window_width = 2000
+#     browser.config.window_height = 1080
+#
+#     yield
+#
+#     browser.quit()
