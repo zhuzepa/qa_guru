556a3bbb0b76b20df86877c321d8d8d17675a7c6
Prince Aleksey <chirkinaa@pgk.ru>
2023-09-06 22:16:56 +0300

    modified files

---
+++ conftest.py
@@ -0,0 +1,10 @@
+from selene import browser, have, be
+import pytest
+
+@pytest.fixture
+def seting_br():
+    browser.config.base_url = 'https://github.com/'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1082
+
+    yield
---
+++ test_github.py
@@ -0,0 +1,11 @@
+from selene import browser, be, by
+
+
+def test_github(seting_br):
+    browser.open('/')
+
+    browser.element('.header-search-button').click()
+    browser.element('#query-builder-test').type('eroshenkoam/allure-example').press_enter()
+    browser.element(by.link_text('eroshenkoam/allure-example')).click()
+    browser.element('#issues-tab').click()
+    browser.element(by.partial_text('#76')).should(be.visible)
